640dcb7e844b24358a3ce259dca2b3fe1656dcd6
Guilherme <guiassishg11@gmail.com>
2023-08-22 19:15:09 -0300

    projeto inicial

---
+++ site/shop/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+class Site (models.Model):
+    nome_card = models.CharField(max_length = 200)
+    description = models.TextField()
+    path = models.ImageField()
+    primeiro_texto = models.TextField()
+    segundo_texto = models.TextField()
+    alunos = models.IntegerField()
+    date_create = models.DateTimeField(default = datetime.now, blank = True)
