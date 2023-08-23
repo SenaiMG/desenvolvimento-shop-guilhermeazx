e994393bbe9cf43fbdc2a07672cbf3f033b6da47
Guilherme <guiassishg11@gmail.com>
2023-08-22 21:46:34 -0300

    mycursor

--- site/shop/connection.py
+++ site/shop/connection.py
@@ -2,3 +2,6 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password = '', host = 'localhost')
 cnx.close()
+
+mycursor = cnx.cursor
+mycursor.execute('CREATE DATABASE mydatabase')
